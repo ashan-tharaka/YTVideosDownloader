2051b26b9e43d58fcaf2dc009e1c4dcb44e875b1
ashan-tharaka <ashantharaka715@gmail.com>
2022-02-16 00:04:29 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+
+app.use(cors());
+
+
+app.listen(4000, '0.0.0.0', () => {
+    console.log('Server Works!!! At Port 4000');
+
+})
+app.get('/download', (req, res) => {
+    var URL = req.query.URL;
+    res.header('Content-Disposition', 'attachment; filename = "video.mp4"');
+
+    ytdl(URL, {
+        format: 'mp4'
+    }).pipe(res);
+
+
+});
